7327397f540287e1c7c942f422d44b557f784aaa
Henry Li <henry1943@163.com>
2018-10-14 11:03:05 +0800

    Add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

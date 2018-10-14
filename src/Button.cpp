5c964c32865ca14fc6e89e467a16bd67d96f67a8
Henry Li <henry1943@163.com>
2018-10-14 20:33:57 +0800

    Implement Button

--- src/Button.cpp
+++ src/Button.cpp
@@ -8,5 +8,39 @@ void Button::begin() {
   pinMode(_pin, INPUT_PULLUP);
 }
 
+void Button::onPress(EventHandler handler) {
+  _pressEventHandler = handler;
+}
+
+void Button::onRelease(EventHandler handler) {
+  _releaseEventHandler = handler;
+}
+
 void Button::update() {
+  auto value = digitalRead(_pin);
+  if (!_pressing) {
+    if (value == LOW && millis() - _lastPress > 300) {
+      _pressing = true;
+      _lastPress = millis();
+      _firePressEvent();
+    }
+  } else {
+    if (value == HIGH) {
+      _pressing = false;
+      _lastPress = millis();
+      _fireReleaseEvent();
+    }
+  }
+}
+
+void Button::_firePressEvent() {
+  if (_pressEventHandler) {
+    _pressEventHandler();
+  }
+}
+
+void Button::_fireReleaseEvent() {
+  if (_releaseEventHandler) {
+    _releaseEventHandler();
+  }
 }
--- src/Button.h
+++ src/Button.h
@@ -2,13 +2,25 @@
 
 #include <Arduino.h>
 
+#include "events.h"
+
 class Button {
 public:
   Button(uint8_t pin);
+
+  void onPress(EventHandler handler);
+  void onRelease(EventHandler handler);
 
   void begin();
   void update();
 
 private:
+  void _firePressEvent();
+  void _fireReleaseEvent();
+
   uint8_t _pin;
+  bool _pressing = false;
+  unsigned long _lastPress = 0;
+  EventHandler _pressEventHandler = NULL;
+  EventHandler _releaseEventHandler = NULL;
 };

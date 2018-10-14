638836b06470ee8aff6a1c905640a3460f134ca4
Henry Li <henry1943@163.com>
2018-10-14 15:32:21 +0800

    Add buttons

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,14 @@
 #include "log.h"
 
 #include "BLEKeyboard.h"
+#include "Button.h"
 #include "KeynotesController.h"
 #include "LaserEmitter.h"
+
+Button previousSlideButton(PREVIOUS_SLIDE_BUTTON_PIN);
+Button nextSlideButton(NEXT_SLIDE_BUTTON_PIN);
+Button laserButton(LASER_BUTTON_PIN);
+LaserEmitter laserEmitter(LASER_EMITTER_PIN);
 
 void setup() {
   Serial.begin(115200);
@@ -17,17 +23,28 @@ void setup() {
   LOG_I("Designed by Henry Li <henry1943@163.com>.");
   LOG_I("Copyright %d. All rights reserved.", 2018);
 
-  LaserEmitter.begin(LASER_EMITTER_PIN);
+  // Initialize buttons and laser emitter.
+  previousSlideButton.begin();
+  nextSlideButton.begin();
+  laserButton.begin();
+  laserEmitter.begin();
 
-  LOG_I("Initializing BLE Device as '%s'...", DEVICE_NAME);
+  LOG_I("Initializing BLE Device '%s'...", DEVICE_NAME);
   BLEDevice::init(DEVICE_NAME);
   LOG_I("Initializing BLE HID services...");
   BLEKeyboard.begin();
 
   BLEKeyboard.startAdvertising();
 
-  LOG_I("Initialization has been done successfully.");
+  LOG_I("Initialization has been <DONE>.");
+}
+
+void updateButtons() {
+  previousSlideButton.update();
+  nextSlideButton.update();
+  laserButton.update();
 }
 
 void loop() {
+  updateButtons();
 }

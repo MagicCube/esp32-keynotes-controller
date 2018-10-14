c3a9590206a1d4aeb063abd9f6f72f73bb867f33
Henry Li <henry1943@163.com>
2018-10-14 14:26:24 +0800

    Change LOG_I() to LOG()

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,24 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
 
-  LOG_I("KeyLight - An ESP32 based BLE Keynote remote controller.");
-  LOG_I("Designed by Henry Li <henry1943@163.com>.");
-  LOG_I("Copyright %d. All rights reserved.", 2018);
+  LOG("KeyLight - An ESP32 based BLE Keynote remote controller.");
+  LOG("Designed by Henry Li <henry1943@163.com>.");
+  LOG("Copyright %d. All rights reserved.", 2018);
 
   LaserEmitter.begin(LASER_EMITTER_PIN);
 
-  LOG_I("Initializing BLE Device as '%s'...", DEVICE_NAME);
+  LOG("Initializing BLE Device as '%s'...", DEVICE_NAME);
   BLEDevice::init(DEVICE_NAME);
-  LOG_I("Initializing BLE HID services...");
+  LOG("Initializing BLE HID services...");
   BLEKeyboard.begin();
 
-  LOG_I("Start BLE advertising...");
+  LOG("Start BLE advertising...");
   BLEKeyboard.startAdvertising();
 
-  LOG_I("Initialization has been done successfully.");
+  LOG("Initialization has been done successfully.");
 }
 
 void loop() {
+  delay(2000);
+  KeynotesController.nextSlide();
 }
